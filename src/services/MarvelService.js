8128b4ea00116901f4619aa9f5bdfb96eb933b35
Sergey <solodoff.s@mail.ru>
2022-02-21 16:01:34 +0300

    random characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,11 +12,21 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters() {
+    getAllCharacters = () => {
         return this.getResource(`${this._apiBase}characters?limit=9&offset=210${this._apiKey}`);
     }
-    getCharacter(id) {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0])
+    }
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
